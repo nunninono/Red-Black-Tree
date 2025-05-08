ddeb8d315caa68dd13f9a795b552d077a7d43f30
Nunno Wakiyama Diniz Carvalho <142551010+nunninono@users.noreply.github.com>
2025-05-08 16:20:05 -0300

    Update RBTree.java

--- Red_Black/src/RedBlackGenerica/RBTree.java
+++ Red_Black/src/RedBlackGenerica/RBTree.java
@@ -14,23 +14,123 @@ public class RBTree <T extends Comparable<T>>{
             this.root.setColor(true);  // setBlack
         }
         else {
-            this.root = insertNode(this.root, value);
+            insertNode(this.root, value);
         }
     }
 
-    public RBNode<T> insertNode (RBNode<T> node, T value) {
+    public void insertNode (RBNode<T> aux, T value) {
+        boolean foi_inserido = false;
+        RBNode<T> novoNo = new RBNode<T> (value);
 
-        while (node != null) {
-            
+        if (foi_inserido == true) {
+            if (novoNo.getPai().isColor() == false) {
+               realizar_operacao(novoNo); 
+            }
         }
-
-        return node;
+        else if (aux.getInfo().compareTo(value) > 0) {
+            // desce para esquerda
+            insertNode(aux.getLeft(), value);
+            if (aux.getLeft() == null) {
+                aux.setLeft(novoNo);
+                novoNo.setPai(aux);
+                foi_inserido = true;
+            }
+        }
+        else {
+            // desce para esquerda
+            insertNode(aux.getRight(), value);
+            if (aux.getRight() == null) {
+                aux.setRight(novoNo);
+                novoNo.setPai(aux);
+                foi_inserido = true;
+            }
+        }
     }
 
-    public void balancear(RBNode<T> k) {
+    public void realizar_operacao(RBNode<T> k) {
         RBNode<T> p = k.getPai();
         RBNode<T> g = p.getPai();
         RBNode<T> s;
+
+        // Verificando quem é o tio (s)
+        if (g.getLeft() == p) {
+            s = g.getRight();
+        }
+        else {
+            s = g.getLeft();
+        }
+
+        // false = red
+        if (s.isColor() == false) {     // Recolorindo
+            recolorir(k, p, g, s);
+        }
+        else {      // Rotação
+            rotacao(k, p, g);
+        }
+    }
+
+    public void recolorir(RBNode<T> k, RBNode<T> p, RBNode<T> g, RBNode<T> s) {
+        k.setColor(false); // red
+        p.setColor(true);   // black
+        if (g != this.root) {
+            g.setColor(false);  // red
+        }
+        s.setColor(true);   // black
+
+        RBNode<T> bisavo;
+        bisavo = g.getPai();
+        if (bisavo.isColor() == false) {
+            RBNode<T> new_k = g;
+            RBNode<T> new_p = new_k.getPai();
+            RBNode<T> new_g = new_p.getPai();
+            RBNode<T> new_s;
+            if (new_g.getLeft() == new_p) {
+                new_s = new_g.getRight();
+            }
+            else {
+                new_s = new_g.getLeft();
+            }
+            recolorir(new_k, new_p, new_g, new_s);
+        }
+    }
+
+    public void rotacao(RBNode<T> k, RBNode<T> p, RBNode<T> g) {
+        if (g.getLeft() == p && p.getLeft() == k) {     // R.S.D
+            // p é o A
+            // k é o B
+            p.setLeft(k.getRight()); 
+            k.setRight(p); 
+            p = k; 
+        }
+        else if (g.getRight() == p && p.getRight() == k) {      // R.S.E
+            // p é o A
+            // k é o B
+            p.setRight(k.getLeft()); 
+            k.setLeft(p); 
+            p = k;  
+        }
+        else if (g.getLeft() == p && p.getRight() == k) {       // R.D.D
+            // k é o C
+            // p é o B
+            // g é o A
+            k = p.getRight(); 
+            p.setRight(k.getLeft()); 
+            k.setLeft(p); 
+            g.setLeft(k.getRight()); 
+            k.setRight(g); 
+            g = k;
+        }
+        else if (g.getRight() == p && p.getLeft() == k) {       // R.D.E
+            // k é o C
+            // p é o B
+            // g é o A
+            k = p.getLeft(); 
+            p.setLeft(k.getRight()); 
+            k.setRight(p);
+            g.setRight(k.getLeft()); 
+            k.setLeft(g); 
+            g = k;
+        }
     }
 
     public void emOrdem() {
